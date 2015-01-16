900b43d9fe9fd8172723aafdde7fdb3d4e35c354
Aurora Nou <auroranou@gmail.com>
2015-01-16 14:29:29 -0500

    add resume changes

--- site.js
+++ site.js
@@ -1,10 +1,8 @@
 $(document).ready(function(){
-
 	$('.portfolio').hover(function(){
 		$(this).find('.portfolio-title').fadeIn('fast');
 	},
 	function(){
 		$(this).find('.portfolio-title').fadeOut('slow');
 	});
-
 });
