a5d59309ecf46cbd47779818ac97b879453081ac
Aurora Nou <auroranou@gmail.com>
2015-01-18 14:43:42 -0500

    clean up file structure

--- site.js
+++ site.js
@@ -3,7 +3,9 @@ $(document).ready(function(){
 	// set landing div (class = top) to full window height
 	var height = $(window).height() - 20 + 'px';
 	$('.top').css('height', height);
-	$('.top').hide().slideDown(1000);
+
+	// slide top div down on homepage
+	$('#index').hide().slideDown(1000);
 
 	// change h1 on hover
 	var intervalID; 
