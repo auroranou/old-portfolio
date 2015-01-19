9fa1a598503c3a4c521e235aa9fab51d2a4581f5
Aurora Nou <auroranou@gmail.com>
2015-01-18 23:31:41 -0500

    add first portfolio page transition

--- site.js
+++ site.js
@@ -23,14 +23,39 @@ $(document).ready(function() {
 		$('.center').fadeOut();
 		$('#index-top').slideUp(1000);
 		$('#page-resume').show();
-		$('#index-main').addClass('hidden');
+		$('#page-index').hide();
 		$('body').animate({ scrollTop: 0}, 'slow');
 		$('#header-name').fadeIn();
 	});
 
-	if (loc ==="#resume") {
-		$('#page-index').addClass('hidden');
-		$('#page-resume').show();
+	// maintain resume page styling and content
+	if (loc === '#resume') {
 		$('#header-name').show();
+		$('#page-resume').show();
+		$('#page-index').addClass('hidden');
+	}
+
+	// transition animation from homepage to portfolio
+	$('a[href="#portfolio-one"]').click(function() {
+		var width = $(window).width();
+		$('#page-index').fadeOut(500);
+		$('#page-portfolio-one').show();
+	});
+
+	// maintain portfolio page styling and content
+	if (loc === '#portfolio-one') {
+		$('#page-index').addClass('hidden');
+		$('#page-portfolio-one').show();
+	}
+
+	// transition from portfolio to homepage
+	$('a[href="#"]').click(function() {
+		$('#page-portfolio-one').fadeOut(500);
+		$('#page-index').fadeIn(500);
+	});
+
+	if (loc === '' || loc === '#') {
+		$('#page-index').show();
+		$('#page-resume').addClass('hidden');
 	}
 });
