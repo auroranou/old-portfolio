3ca9c9bedfc38bd6dd9cf3a2b519bcc5fba3753c
Aurora Nou <auroranou@gmail.com>
2015-02-12 13:56:37 -0500

    refactor index page animation to use css instead of javascript

--- assets/javascripts/site.js
+++ assets/javascripts/site.js
@@ -2,14 +2,6 @@
 $(document).ready(function() {
 	var loc = window.location.hash;
 
-	// change h1 on hover
-	var intervalID; 
-	$('#name').html('_');
-	intervalID = setInterval(function() {
-		$('#name').toggleClass('color-change');
-	}, 500);
-	setInterval();
-
 	// transition animation from homepage to resume
 	$('a[href="#resume"]').click(function() {
 		$('.center').fadeOut();
