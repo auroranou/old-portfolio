351af2ce69fbbc02c68242d2f723fbd82381666a
Aurora Nou <auroranou@gmail.com>
2015-03-17 13:06:38 -0400

    navigate site using arrow keys

--- assets/javascripts/site.js
+++ assets/javascripts/site.js
@@ -1,5 +1,16 @@
 $(document).ready(function() {
+  console.log(window.location);
   if (window.location.pathname != '/resume/') {
-    
+    $('body').keydown(function(e) {
+      var code = e.keyCode || e.which;
+      if (code == 39) { //right arrow
+        var next = $(this).find('a.next');
+        window.location.href = next[0]['href'];
+      }
+      else if (code == 37) { //left arrow
+        var prev = $(this).find('a.prev');
+        window.location.href = prev[0]['href'];
+      }
+    });
   }
 });
